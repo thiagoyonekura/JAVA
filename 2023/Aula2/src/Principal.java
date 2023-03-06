1dc2e02a317a0269fa1654c1fe1a9037fc87c809
thiagoyonekura <104376017+thiagoyonekura@users.noreply.github.com>
2023-03-06 15:14:48 -0300

    Principal.java

--- 2023/Aula2/src/Principal.java
+++ 2023/Aula2/src/Principal.java
@@ -1,48 +1,40 @@
 import java.util.Scanner;
 
 public class Principal {
-	//função == método
-	public static void main(String[] args) {
-		/*
-		Scanner leitor = new Scanner(System.in);
-		System.out.println("Digite um valor");
-		int num1 = leitor.nextInt();
-		System.out.println("Digite outro valor");
-		int num2 = leitor.nextInt();
-		
-		int media = (num1 + num2)/2;
-		if(media >=6) {
-			System.out.println("Aprovado");
-		}else if(media < 6 && media >=1) {
-			System.out.println("Exame");
-		}else {
-			System.out.println("Reprovado");
-		}
-		
-		leitor.close();
-		
-		int num1 = 10;
-		int num2 = 20;
-		int num3 = 45;
-		int media = (num1 + num2 + num3)/3;
-		System.out.println("A média eh " + media);
-		*/
-		Scanner leitor = new Scanner(System.in);
-		System.out.println("Digite o lado A do triangulo");
-		float ladoA = leitor.nextFloat();
-		System.out.println("Digite o lado B do triangulo");
-		float ladoB = leitor.nextFloat();
-		System.out.println("Digite o lado C do triangulo");
-		float ladoC = leitor.nextFloat();
-		System.out.println(ladoA + "" + ladoB + "" + ladoC );
-		if(ladoA == ladoB && ladoB == ladoC) {
-			System.out.println("O triangulo eh equilatero");
-		}else if(ladoA != ladoB && ladoB != ladoC) {
-			System.out.println("O triangulo eh Escaleno");
-		}else {
-			System.out.println("O triangulo eh Isósceles");
-		}
-		
-	}
-	
+    public static void main(String []args) {
+        // Exercício incremento
+        int i =10;
+        int j = 2;
+        int x = 0;
+        int y = 0;
+        j++; // pós incremento
+        ++i; // pré incremento
+        x = i++ + j;
+        y = ++j + ++i;
+        // Exemplo de concatenção com o operado +
+        System.out.println("X = "+ x + "\t Y = " + y);
+        /*
+         * Leia 3 valores que representam o lado de um triângulo
+         * e diga se o triangulo é isóceles, equilatero, escaleno.
+         */
+        // Leitura de dados do usuário
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o lado A do triangulo");
+        float ladoA = leitor.nextFloat();
+        System.out.println("Digite o lado B do triangulo");
+        float ladoB = leitor.nextFloat();
+        System.out.println("Digite o lado C do triangulo");
+        float ladoC = leitor.nextFloat();
+        System.out.println(ladoA +" "+ladoB + " " + ladoC )
+        //float soma = ladoA + ladoB;
+        if(ladoA == ladoB && ladoB == ladoC) {
+            System.out.println("Equilatero");
+        }else if(ladoA != ladoB && ladoB != ladoC) {
+            System.out.println("Escaleno");
+        }else {
+            System.out.println("Isóceles");
+        }
+        leitor.close();
+    }
+
 }
